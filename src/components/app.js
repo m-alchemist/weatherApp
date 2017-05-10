72149b8f40b0c50f3d22f860cc7dcb9027f072d4
m-alchemist <alkhamis@email.arizona.edu>
2017-05-10 03:22:19 -0700

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
-
+import SearchBar from '../containers/search_bar';
+import WeatherList from '../containers/weather_list';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <div className='header-cont'>
+          <div className='text-center title'>
+            <div className='title2'>
+              Weather
+            </div>
+            <div className='title3'>
+              App
+            </div>
+          </div>
+          <SearchBar/>
+
+            <div className='header-cont2'>
+            <div className='tableHead'>
+            <p className='headTitle city'> City </p>
+            <p className='headTitle temp'> Temperature (K) </p>
+            <p className='headTitle press'> Pressure (hPa)   </p>
+              <p className='headTitle hum'> Humidity (%)    </p>
+            </div>
+            </div>
+
+        </div>
+
+        <div className='weatherList'>
+
+          <WeatherList/>
+        </div>
+      </div>
     );
   }
 }
